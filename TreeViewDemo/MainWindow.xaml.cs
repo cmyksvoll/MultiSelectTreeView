1616a6e497770940792bc1011801a027550bc798
Christian Myksvoll <chrimyk@acos.no>
2015-07-10 13:54:06 +0200

    First version

---
+++ TreeViewDemo/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace TreeViewDemo
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            SelectedNodes = new ObservableCollection<TreeItemViewModel>();
+            SelectedNodes.CollectionChanged += SelectedNodes_CollectionChanged;
+
+            RootNodes = BuildTreeModel();
+
+            InitializeComponent();
+        }
+
+        private static List<TreeItemViewModel> BuildTreeModel()
+        {
+            return new List<TreeItemViewModel>
+            {
+                new TreeItemViewModel("Node 1")
+                {
+                    IsExpanded = true,
+                    Children = new List<TreeItemViewModel>
+                    {
+                        new TreeItemViewModel("Node 1.1"),
+                        new TreeItemViewModel("Node 1.2")
+                        {
+                            Children = new List<TreeItemViewModel>
+                            {
+                                new TreeItemViewModel("Node 1.2.1"),
+                                new TreeItemViewModel("Node 1.2.2"),
+                                new TreeItemViewModel("Node 1.2.3"),
+                                new TreeItemViewModel("Node 1.2.4"),
+                                new TreeItemViewModel("Node 1.2.5"),
+                                new TreeItemViewModel("Node 1.2.6")
+                            }
+                        }
+                    }
+                },
+                new TreeItemViewModel("Node 2")
+                {
+                    Children = new List<TreeItemViewModel>
+                    {
+                        new TreeItemViewModel("Node 2.2.1"),
+                        new TreeItemViewModel("Node 2.2.2"),
+                        new TreeItemViewModel("Node 2.2.3"),
+                        new TreeItemViewModel("Node 2.2.4")
+                    }
+                }
+            };
+        }
+
+        public List<TreeItemViewModel> RootNodes { get; set; }
+
+        public ObservableCollection<TreeItemViewModel> SelectedNodes { get; set; }
+
+        void SelectedNodes_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            NumberOfSelectedNodes.Text = SelectedNodes.Count.ToString();
+        }
+    }
+}
---
+++ TreeViewDemo/TreeItemViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq.Expressions;
+
+namespace TreeViewDemo
+{
+    public class TreeItemViewModel : INotifyPropertyChanged
+    {
+        public TreeItemViewModel(string displayName)
+        {
+            DisplayName = displayName;
+        }
+
+        private bool _isExpanded;
+        private bool _isSelected;
+
+        public string DisplayName { get; set; }
+
+        public bool IsExpanded
+        {
+            get { return _isExpanded; }
+            set
+            {
+                _isExpanded = value;
+                OnPropertyChanged(() => IsExpanded);
+            }
+        }
+
+        public virtual bool IsSelected
+        {
+            get { return _isSelected; }
+            set
+            {
+                _isSelected = value;
+                OnPropertyChanged(() => IsSelected);
+            }
+        }
+
+        public List<TreeItemViewModel> Children { get; set; }        
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged<T>(Expression<Func<T>> propertyExpression)
+        {
+            var body = propertyExpression.Body as MemberExpression;
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(body.Member.Name));
+        }
+    }
+}
